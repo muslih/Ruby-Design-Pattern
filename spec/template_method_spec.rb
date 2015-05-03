908b6fb7bc2ddc83894e07ad23c4622b455b1153
Muslih <muslihzarth@gmail.com>
2015-05-03 16:13:23 +0800

    Basic hero class

--- spec/template_method_spec.rb
+++ spec/template_method_spec.rb
@@ -4,6 +4,10 @@ require_relative '../lib/template_method'
 describe "Template Method Pattern" do
 	let(:hero) { Hero.new }
 
-	it "Default damagin rating about 10"
-	
+	it "Default damagin rating about 10" do
+		expect(hero.damage).to eq(10)
+	end
+	it "Can attack" do
+		expect(hero.attack).to eq("Attacked dealing 10 damage")
+	end
 end
